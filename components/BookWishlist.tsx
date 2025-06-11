2006590d2f51492c417528af5cbe1c6f8360e750
AdrianGerman <adrian.german1019@gmail.com>
2025-06-11 08:10:13 -0600

    add Book in Book wishlist

--- components/BookWishlist.tsx
+++ components/BookWishlist.tsx
@@ -1,10 +1,27 @@
+import { useState } from "react"
+import { Book } from "@/types/books"
 import BookForm from "./BookForm"
 
 export default function BookWishlist() {
+  const [books, setBooks] = useState<Book[]>([])
+
+  const addBook = (book: Book) => {
+    setBooks([book, ...books])
+  }
+
+  // const deleteBook = (id: string) => {
+  //   setBooks(books.filter((book) => book.id !== id))
+  // }
+  // const updateBook = (updatedBook: Book) => {
+  //   setBooks(
+  //     books.map((book) => (book.id === updatedBook.id ? updatedBook : book))
+  //   )
+  // }
+
   return (
     <>
       <div>
-        <BookForm />
+        <BookForm onAdd={addBook} />
       </div>
     </>
   )
