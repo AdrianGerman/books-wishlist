c221fa8fcb329ae85b2f7055c85b7b7842c5a22d
AdrianGerman <adrian.german1019@gmail.com>
2025-06-12 07:35:20 -0600

    changes in wishlist and page

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,11 @@ import BookWishlist from "@/components/BookWishlist"
 
 export default function Page() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-4">
-      <h1 className="text-2xl font-bold mb-4">Book Wishlist</h1>
+    <main className="flex flex-col items-center justify-center min-h-screen p-4 bg-gray-100 dark:bg-gray-900">
+      <h1 className="text-4xl font-bold mb-6 text-center bg-gradient-to-r from-purple-500 to-pink-500 text-transparent bg-clip-text">
+        📚 Book Wishlist App
+      </h1>
       <BookWishlist />
-    </div>
+    </main>
   )
 }
--- components/BookWishlist.tsx
+++ components/BookWishlist.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import { Book } from "@/types/books"
 import BookForm from "./BookForm"
+import BookList from "./BookList"
 
 export default function BookWishlist() {
   const [books, setBooks] = useState<Book[]>([])
@@ -9,9 +10,9 @@ export default function BookWishlist() {
     setBooks([book, ...books])
   }
 
-  // const deleteBook = (id: string) => {
-  //   setBooks(books.filter((book) => book.id !== id))
-  // }
+  const deleteBook = (id: string) => {
+    setBooks(books.filter((book) => book.id !== id))
+  }
   // const updateBook = (updatedBook: Book) => {
   //   setBooks(
   //     books.map((book) => (book.id === updatedBook.id ? updatedBook : book))
@@ -19,10 +20,12 @@ export default function BookWishlist() {
   // }
 
   return (
-    <>
-      <div>
+    <div className="w-full max-w-5xl mx-auto">
+      <div className="flex  justify-between items-center mb-6">
+        <h2 className="text-3xl font-bold">Mi Wishlist</h2>
         <BookForm onAdd={addBook} />
       </div>
-    </>
+      <BookList books={books} onDelete={deleteBook} />
+    </div>
   )
 }
